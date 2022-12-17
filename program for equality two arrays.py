a8fdaddf4c6b0c5e7ec97c755fe40b64db68201e
Manasa <108573533+manasabojja123@users.noreply.github.com>
2022-12-17 19:29:28 +0530

    Add files via upload

---
+++ program for equality two arrays.py
@@ -0,0 +1,34 @@
+l1=[1,2,3,1,4,1,1]
+l2=[1,2,3,1,4,1,1]
+n1=[]
+n2=[]
+n3=[]
+n4=[]
+for i in l1:
+    if(i not in n1):
+        n1.append(i)
+for i in l2:
+    if(i not in n2):
+        n2.append(i)
+n1.sort()
+n2.sort()
+if(n1==n2):
+    for i in n1:
+        n3.append(l1.count(i))
+        n4.append(l2.count(i))
+    
+    n3.sort()
+    n4.sort()
+    if(n3==n4):
+        print(True)
+
+    else:
+        print(False)
+else:
+    print(False)
+    
+
+
+    
+        
+            
